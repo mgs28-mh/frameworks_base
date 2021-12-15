b8cffcd8d255b33830bfa36fe8f16c5e96d23e3c
Aryan Gupta <guptaaryan189@gmail.com>
2021-12-15 13:56:39 +0530

    SystemUI: AuthController: Fix a NPE on some devices
    java.lang.RuntimeException: Unable to get provider org.pixelexperience.android.systemui.keyguard.KeyguardSliceProviderGoogle: java.lang.RuntimeException: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean android.hardware.fingerprint.FingerprintManager.isPowerbuttonFps()' on a null object reference
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread.installProvider(ActivityThread.java:7427)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread.installContentProviders(ActivityThread.java:6939)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread.handleBindApplication(ActivityThread.java:6710)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread.access$1500(ActivityThread.java:247)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2053)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:201)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:288)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:7839)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1003)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: Caused by: java.lang.RuntimeException: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean android.hardware.fingerprint.FingerprintManager.isPowerbuttonFps()' on a null object reference
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.systemui.SystemUIFactory.createFromConfig(SystemUIFactory.java:77)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.systemui.SystemUIFactory.createFromConfig(SystemUIFactory.java:56)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.systemui.SystemUIAppComponentFactory.lambda$instantiateProviderCompat$1(SystemUIAppComponentFactory.java:88)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.systemui.SystemUIAppComponentFactory.$r8$lambda$qcrPqG7eXiVK_IrWSM3yDqdi2js(Unknown Source:0)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.systemui.SystemUIAppComponentFactory$$ExternalSyntheticLambda0.onContextAvailable(Unknown Source:2)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at com.android.systemui.keyguard.KeyguardSliceProvider.onCreateSliceProvider(KeyguardSliceProvider.java:304)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at org.pixelexperience.android.systemui.keyguard.KeyguardSliceProviderGoogle.onCreateSliceProvider(KeyguardSliceProviderGoogle.java:93)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at androidx.slice.SliceProvider.onCreate(SliceProvider.java:211)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.content.ContentProvider.attachInfo(ContentProvider.java:2404)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.content.ContentProvider.attachInfo(ContentProvider.java:2374)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at androidx.slice.SliceProvider.attachInfo(SliceProvider.java:250)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at androidx.slice.compat.SliceProviderWrapperContainer$SliceProviderWrapper.attachInfo(SliceProviderWrapperContainer.java:74)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	at android.app.ActivityThread.installProvider(ActivityThread.java:7422)
    12-05 13:06:32.938  1763  1763 E AndroidRuntime: 	... 11 more

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -479,10 +479,8 @@ public class AuthController extends SystemUI implements CommandQueue.Callbacks,
                     (float) faceAuthLocation[1]);
         }
 
-        boolean isPowerbuttonFps =
-                mFingerprintManager != null && mFingerprintManager.isPowerbuttonFps();
-
-        mFingerprintLocation = new PointF(
+        boolean isPowerbuttonFps = mFingerprintManager != null && mFingerprintManager.isPowerbuttonFps();
+                mFingerprintLocation = new PointF(
                 DisplayUtils.getWidth(mContext) / (isPowerbuttonFps ? 1 : 2),
                 mContext.getResources().getDimensionPixelSize(
                 com.android.systemui.R.dimen.physical_fingerprint_sensor_center_screen_location_y));
